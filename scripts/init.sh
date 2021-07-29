c8c93edca88ed8a97af9964946ab29ff3bbf284f
Olivier Bourdon <olivier.bourdon@squarescale.com>
2021-07-29 08:33:58 +0200

    Uniformize un-archiving of files and factorize some code

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "########## Installing Docker..."
+echo "########## Updating system..."
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
+echo "########## Installing Docker..."
 sudo apt-get remove docker docker-engine docker.io
 echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
@@ -27,24 +28,21 @@ echo "########## Installing Nomad..."
 NOMAD_VERSION=1.1.2
 cd /tmp/
 curl -sSL https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip -o nomad.zip
-unzip nomad.zip
-sudo mv nomad /usr/local/bin/
+sudo unzip -q nomad.zip -d /usr/local/bin/
 sudo mkdir -p /etc/nomad.d
 sudo chmod a+w /etc/nomad.d
 
 
 echo "########## Installing Consul..."
 CONSUL_VERSION=1.10.1
-curl -sSL https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip > consul.zip
-unzip consul.zip
-sudo mv consul /usr/local/bin/
-sudo mkdir -p /var/lib/consul
+curl -sSL https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip -o consul.zip
+sudo unzip -q consul.zip -d /usr/local/bin/
+sudo mkdir -p /var/lib/consul /etc/consul.d
 sudo chmod -R 775 /var/lib/consul
-sudo mkdir /etc/consul.d
 
 # https://discuss.hashicorp.com/t/failed-to-find-plugin-bridge-in-path/3095
 CNI_PLUGINS_VERSION=0.9.1
 echo "########## Enable CNI Plugin"
-curl -L -o cni-plugins.tgz https://github.com/containernetworking/plugins/releases/download/v${CNI_PLUGINS_VERSION}/cni-plugins-linux-amd64-v${CNI_PLUGINS_VERSION}.tgz
+curl -sSL https://github.com/containernetworking/plugins/releases/download/v${CNI_PLUGINS_VERSION}/cni-plugins-linux-amd64-v${CNI_PLUGINS_VERSION}.tgz -o cni-plugins.tgz
 sudo mkdir -p /opt/cni/bin
 sudo tar -C /opt/cni/bin -xzf cni-plugins.tgz
