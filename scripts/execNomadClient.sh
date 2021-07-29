459193176a9ca44e5afd4f4d393363d889cd4d87
Olivier Bourdon <olivier.bourdon@squarescale.com>
2021-07-29 14:27:41 +0200

    Uniformize config files creation and contents for Nomad, use hostnames as node
    Also add raw_exec driver support

--- scripts/execNomadClient.sh
+++ scripts/execNomadClient.sh
@@ -6,7 +6,25 @@ DEFAULT_IP=172.17.0.2
 IP="${1:-$DEFAULT_IP}"
 SERVER_IP="${2:-DEFAULT_SERVER_IP}"
 
-echo "[Unit]
+cat >/etc/nomad.d/nomad-client.hcl <<EOF
+data_dir  = "/var/lib/nomad"
+
+bind_addr = "0.0.0.0" # the default
+
+client {
+  enabled = true
+  servers = ["${SERVER_IP}"]
+}
+
+plugin "raw_exec" {
+  config {
+    enabled = true
+  }
+}
+EOF
+
+cat >/etc/systemd/system/nomad.service <<EOF
+[Unit]
 Description=Nomad Service Discovery Agent
 Documentation=https://www.nomad.io/
 After=network-online.target
@@ -14,19 +32,17 @@ Wants=network-online.target
 
 [Service]
 ExecStart=/usr/local/bin/nomad agent \
-  -node=$IP \
-  -bind=$IP \
-  -client \
-  -network-interface=eth1 \
-  -data-dir=/var/lib/nomad \
-  -retry-join=$SERVER_IP \
+  -config=/etc/nomad.d \
   -encrypt=TeLbPpWX41zMM3vfLwHHfQ==
-
 ExecReload=/bin/kill -HUP $MAINPID
 Restart=on-failure
 
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/nomad.service
+WantedBy=multi-user.target
+EOF
 
 systemctl enable nomad.service
 systemctl start nomad
+
+echo "export NOMAD_ADDR=http://$IP:4646" >> /home/vagrant/.bashrc
+echo "NOMAD_ADDR=http://$IP:4646" >> /etc/environment
